dc6c6d3b9bd99ed62d80922741974a3988fd0514
junkevin88 <junkevin88@gmail.com>
2023-01-25 12:17:45 +0700

    resolve conflict

--- src/main/java/com/synergy/productService/service/impl/KostServiceImpl.java
+++ src/main/java/com/synergy/productService/service/impl/KostServiceImpl.java
@@ -1,13 +1,13 @@
 package com.synergy.productService.service.impl;
 
 import com.cloudinary.Cloudinary;
-import com.synergy.productService.dto.KostModel;
 import com.synergy.productService.entity.Kost;
-import com.synergy.productService.entity.Profile;
 import com.synergy.productService.repository.KostRepo;
 import com.synergy.productService.repository.ProfileRepo;
 import com.synergy.productService.service.KostService;
 import com.synergy.productService.util.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +18,8 @@ import java.util.Map;
 
 @Service
 public class KostServiceImpl implements KostService {
+
+    private static Logger logger = LoggerFactory.getLogger(KostServiceImpl.class);
 
     @Autowired
     public Response templateResponse;
@@ -56,7 +58,20 @@ public class KostServiceImpl implements KostService {
         String secureUrl = cloudinary.url().secure(true).generate(publicId);
         return secureUrl;
     }
+    @Override
+    public Map getById(Long id) {
+        try {
+            Kost checkingData = kostRepo.getById(id);
+            if (checkingData == null) {
+                return templateResponse.notFoundError("Data cannot be found!");
+            }
+            return templateResponse.resSuccess(checkingData,"success", 200);
 
+        } catch (Exception e) {
+            logger.error("Error get by id, {} " + e);
+            return templateResponse.clientError("Error get by id: " + e);
+        }
+    }
 
 }
 
