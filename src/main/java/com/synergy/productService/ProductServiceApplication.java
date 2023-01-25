0e42cc473a3e814bb93de84868379b3a6135dc07
junkevin88 <junkevin88@gmail.com>
2023-01-25 19:53:54 +0700

    update add swagger

--- src/main/java/com/synergy/productService/ProductServiceApplication.java
+++ src/main/java/com/synergy/productService/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.synergy.productService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
