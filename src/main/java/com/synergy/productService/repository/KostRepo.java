812fb5eefa8e3d14660ade37f2739a6e9ecc61be
Agustinus <agustin22.tm@gmail.com>
2023-02-06 23:48:20 +0700

    Revise get list kost by sort filter area and get detail kost by id

--- src/main/java/com/synergy/productService/repository/KostRepo.java
+++ src/main/java/com/synergy/productService/repository/KostRepo.java
@@ -39,22 +39,25 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
 
     @Query(nativeQuery = true, value = "select k.kost_id, k.kost_name, k.city, k.address, k.province, pr.price, pr.duration_type, k.kost_type_man,\n" +
             "k.kost_type_mixed , k.kost_type_woman, k.front_building_photo  from kost k\t " +
-            "\tleft join room r on k.kost_id = r.kost_id and r.deleted_at is null\n" +
+            "\tleft join room r on k.kost_id = r.kost_id and r.deleted_at is null and r.enabled = true\n" +
             "\tleft join facility fa on fa.facility_id = r.facility_id and fa.deleted_at is null\n" +
             "\tleft join price pr on  pr.room_id = r.room_id and pr.deleted_at is null\n" +
-            "\twhere k.deleted_at is null " +
-            "\tand k.enabled = true \n" +
-            "\tand (( fa.ac = :ac) \n" +
-            "\tor ( fa.pillow = :pillow ) \n" +
-            "\tor ( fa.fan = :fan) \n" +
-            "\tor ( fa.furniture = :furniture) \n" +
+            "\twhere k.deleted_at is null \n" +
+            "\tand k.enabled is true \n" +
+            "\tand (lower(k.province) like (%:province%) \n"  +
+            "\tand lower(k.city)  like (%:city%) \n" +
+            "\tand (pr.price between :price_minimum and :price_maximum)) \n" +
+            "\tor ( (fa.ac = :ac) \n" +
+            "\tor (fa.pillow = :pillow)  \n" +
+            "\tor (fa.fan = :fan) \n" +
+            "\tor (fa.furniture = :furniture) \n" +
             "\tor (fa.shower = :shower) \n" +
             "\tor (fa.sitting_closet = :sitting_closet) \n" +
             "\tor (fa.springbed = :springbed) \n" +
             "\tor (fa.table_learning = :table_learning) \n" +
             "\tor (fa.water_heater = :water_heater) \n" +
             "\tor (fa.inside_bathroom = :inside_bathroom) \n" +
-            "\tor (fa.non_sitting_closet = :non_sitting_closet) \n" +
+            "\tor (fa.non_sitting_closet = :non_sitting_closet)\n" +
             "\tor (fa.outside_bathroom = :outside_bathroom) \n" +
             "\tor (fa.windows = :windows) \n" +
             "\tor (fa.room_tv = :room_tv) \n" +
@@ -69,14 +72,11 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
             "\tor (k.kitchen = :kitchen) \n" +
             "\tor (k.living_room = :living_room) \n" +
             "\tor (k.parking_car = :parking_car) \n" +
-            "\tor (k.parking_motorcycle = :parking_motorcycle))" +
-            "\tand lower(k.province) like (%:province%) \n"  +
-            "\tand lower(k.city)  like (%:city%)" +
-            "\tand (k.kost_type_man = :kost_type_man) \n" +
-            "\tand (k.kost_type_woman = :kost_type_woman) \n" +
-            "\tand (k.kost_type_mixed = :kost_type_mixed) \n" +
+            "\tor (k.parking_motorcycle = :parking_motorcycle))\n" +
             "\tand (pr.duration_type = :duration_type) \n" +
-            "\tand (pr.price >= :price_minimum and pr.price <= :price_maximum)")
+            "\tand ((k.kost_type_man = :kost_type_man) \n" +
+            "\tand (k.kost_type_woman = :kost_type_woman) \n" +
+            "\tand (k.kost_type_mixed = :kost_type_mixed))" )
     List<Map<String, Object>> getKostByFilterSortAndAreaWithPagination(@Param("ac") Boolean ac,
                                                                        @Param("pillow") Boolean pillow,
                                                                        @Param("fan") Boolean fan,
@@ -124,13 +124,11 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
     List<Map<String, Object>> getKostBySearchWithPagination(@Param("keyword") String keyword);
 
 
-    @Query(value = "select \n" +
-            "k.kost_id,\n" +
+    @Query(value = "select distinct \n" +
+            "\tk.kost_id,\n" +
             "\tk.kost_name,\n" +
             "\tk.city,\n" +
             "\tk.address,\n" +
-            "\tpr.price,\n" +
-            "\tpr.duration_type,\n" +
             "\tk.kost_type_man,\n" +
             "\tk.kost_type_mixed,\n" +
             "\tk.kost_type_woman,\n" +
@@ -150,12 +148,15 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
             "\tk.refrigerator,\n" +
             "\tk.water,\n" +
             "\tk.wifi, \n" +
+            "\tpr.price,\n" +
+            "\tpr.duration_type,\n" +
             "\tr.room_name,\n" +
             "\tr.inside_room_photo,\n" +
+            "\tr.available_room,\n" +
+            "\tr.room_id,\n" +
+            "\tr.size_room,\n" +
             "--\tfacility\n" +
             "\tf.*,\n" +
-            "\tr.available_room,\n" +
-            "\tr.size_room,\n" +
             "--\trule\n" +
             "\tr2.identity_card,\n" +
             "\tr2.include_electricity,\n" +
@@ -169,8 +170,8 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
             "\tr2.restricted_night,\n" +
             "\tr2.rule_name" +
             "\tfrom\n" +
-            "\t kost k \n" +
-            "left join room r on k.kost_id = r.kost_id and r.deleted_at is null\n" +
+            "\t room r \n" +
+            "left join kost k on k.kost_id = r.kost_id and r.deleted_at is null\n" +
             "left join price pr on pr.room_id = r.room_id and pr.deleted_at is null \n" +
             "left join facility f on f.facility_id = r.facility_id and f.deleted_at is null\n" +
             "left join kost_rule kr on kr.kost_id = k.kost_id \n" +
