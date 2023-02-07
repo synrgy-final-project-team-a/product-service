b5a4e873e83bda8f4e5697f2a4fdbaf8f01f3f44
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-02-07 23:32:34 +0700

    try solve filter sort

--- src/main/java/com/synergy/productService/repository/KostRepo.java
+++ src/main/java/com/synergy/productService/repository/KostRepo.java
@@ -39,7 +39,7 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
         @Query(nativeQuery = true, value = "select k.kost_id, k.kost_name, k.city, k.address, k.province, pr.price, pr.duration_type, k.kost_type_man,\n"
                         +
                         "k.kost_type_mixed , k.kost_type_woman, k.front_building_photo  from kost k\t " +
-                        "\tleft join room r on k.kost_id = r.kost_id and r.deleted_at is null and r.enabled = true\n" +
+                        "\tleft join room r on k.kost_id = r.kost_id and r.deleted_at is null and r.enabled = true and r.price = and r.price = (select min(price) from room ro where k.kost_id = ro.kost_id)\n" +
                         "\tleft join facility fa on fa.facility_id = r.facility_id and fa.deleted_at is null\n" +
                         "\tleft join price pr on  pr.room_id = r.room_id and pr.deleted_at is null\n" +
                         "\twhere k.deleted_at is null \n" +
@@ -47,36 +47,36 @@ public interface KostRepo extends JpaRepository<Kost, Long> {
                         "\tand (lower(k.province) like (%:province%) \n" +
                         "\tand lower(k.city)  like (%:city%) \n" +
                         "\tand (pr.price between :price_minimum and :price_maximum)) \n" +
-                        "\tor ( (fa.ac = :ac) \n" +
-                        "\tor (fa.pillow = :pillow)  \n" +
-                        "\tor (fa.fan = :fan) \n" +
-                        "\tor (fa.furniture = :furniture) \n" +
-                        "\tor (fa.shower = :shower) \n" +
-                        "\tor (fa.sitting_closet = :sitting_closet) \n" +
-                        "\tor (fa.springbed = :springbed) \n" +
-                        "\tor (fa.table_learning = :table_learning) \n" +
-                        "\tor (fa.water_heater = :water_heater) \n" +
-                        "\tor (fa.inside_bathroom = :inside_bathroom) \n" +
-                        "\tor (fa.non_sitting_closet = :non_sitting_closet)\n" +
-                        "\tor (fa.outside_bathroom = :outside_bathroom) \n" +
-                        "\tor (fa.windows = :windows) \n" +
-                        "\tor (fa.room_tv = :room_tv) \n" +
-                        "\tor (k.kost_tv = :kost_tv) \n" +
-                        "\tor (k.electric = :electric) \n" +
-                        "\tor (k.laundry = :laundry) \n" +
-                        "\tor (k.refrigerator = :refrigerator) \n" +
-                        "\tor (k.water = :water) \n" +
-                        "\tor (k.wifi = :wifi) \n" +
-                        "\tor (k.dispenser = :dispenser) \n" +
-                        "\tor (k.drying_ground = :drying_ground) \n" +
-                        "\tor (k.kitchen = :kitchen) \n" +
-                        "\tor (k.living_room = :living_room) \n" +
-                        "\tor (k.parking_car = :parking_car) \n" +
-                        "\tor (k.parking_motorcycle = :parking_motorcycle))\n" +
+                        "\tand ( (fa.ac = :ac or :ac = false) \n" +
+                        "\tand (fa.pillow = :pillow or :pillow = false)  \n" +
+                        "\tand (fa.fan = :fan or :fan = false) \n" +
+                        "\tand (fa.furniture = :furniture = :furniture = false) \n" +
+                        "\tand (fa.shower = :shower or :shower = false) \n" +
+                        "\tand (fa.sitting_closet = :sitting_closet or :sitting_closet = false) \n" +
+                        "\tand (fa.springbed = :springbed or :springbed = false) \n" +
+                        "\tand (fa.table_learning = :table_learning or :table_learning = false) \n" +
+                        "\tand (fa.water_heater = :water_heater or :water_heater = false) \n" +
+                        "\tand (fa.inside_bathroom = :inside_bathroom or :inside_bathroom = false) \n" +
+                        "\tand (fa.non_sitting_closet = :non_sitting_closet or :non_sitting_closet = false)\n" +
+                        "\tand (fa.outside_bathroom = :outside_bathroom or :outside_bathroom = false) \n" +
+                        "\tand (fa.windows = :windows or :windows = false) \n" +
+                        "\tand (fa.room_tv = :room_tv or :room_tv = false) \n" +
+                        "\tand (k.kost_tv = :kost_tv or :kost_tv = false) \n" +
+                        "\tand (k.electric = :electric or :electric = false) \n" +
+                        "\tand (k.laundry = :laundry or :laundry = false) \n" +
+                        "\tand (k.refrigerator = :refrigerator = or :refrigerator = false) \n" +
+                        "\tand (k.water = :water or :water = false) \n" +
+                        "\tand (k.wifi = :wifi or :wifi = false) \n" +
+                        "\tand (k.dispenser = :dispenser or :dispenser = false) \n" +
+                        "\tand (k.drying_ground = :drying_ground or :drying_ground = false) \n" +
+                        "\tand (k.kitchen = :kitchen or :kitchen = false) \n" +
+                        "\tand (k.living_room = :living_room or :living_room = false) \n" +
+                        "\tand (k.parking_car = :parking_car or :parking_car = false) \n" +
+                        "\tand (k.parking_motorcycle = :parking_motorcycle or :parking_motorcycle = false))\n" +
                         "\tand (pr.duration_type = :duration_type) \n" +
-                        "\tand ((k.kost_type_man = :kost_type_man) \n" +
-                        "\tand (k.kost_type_woman = :kost_type_woman) \n" +
-                        "\tand (k.kost_type_mixed = :kost_type_mixed))")
+                        "\tand ((k.kost_type_man = :kost_type_man or :kost_type_man = false) \n" +
+                        "\tand (k.kost_type_woman = :kost_type_woman or :kost_type_woman = false) \n" +
+                        "\tand (k.kost_type_mixed = :kost_type_mixed or :kost_type_mixed = false))")
         List<Map<String, Object>> getKostByFilterSortAndAreaWithPagination(@Param("ac") Boolean ac,
                         @Param("pillow") Boolean pillow,
                         @Param("fan") Boolean fan,
